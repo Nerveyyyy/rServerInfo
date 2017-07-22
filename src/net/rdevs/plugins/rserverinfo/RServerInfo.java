fe283e7ed689977c89698ec125a727ccb67480e3
Pettless <JBCrafted@outlook.com>
2017-07-22 16:52:55 +0100

    Reload + New Commands
    Added /rserverinfo - Shows plugin info + reload command
    
    » /twitter - shows twitter info
    » /forums - shows server forums
    
    *Updated config.yml

--- src/net/rdevs/plugins/rserverinfo/RServerInfo.java
+++ src/net/rdevs/plugins/rserverinfo/RServerInfo.java
@@ -2,14 +2,18 @@ package net.rdevs.plugins.rserverinfo;
 
 import java.io.File;
 
-import org.bukkit.ChatColor;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.FileConfigurationOptions;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import net.rdevs.plugins.rserverinfo.commands.DiscordCommand;
+import net.rdevs.plugins.rserverinfo.commands.ForumsCommand;
 import net.rdevs.plugins.rserverinfo.commands.TeamSpeakCommand;
+import net.rdevs.plugins.rserverinfo.commands.TwitterCommand;
 import net.rdevs.plugins.rserverinfo.commands.WebsiteCommand;
 
 public class RServerInfo extends JavaPlugin implements Listener {
@@ -25,7 +29,6 @@ public class RServerInfo extends JavaPlugin implements Listener {
 	config = getConfig();
 	config.options().copyDefaults(true);
 	conf = new File(getDataFolder(), "config.yml");
-	saveDefaultConfig();
 	saveConfig();
 	saveDefaultConfig();
 	}
@@ -34,6 +37,9 @@ public class RServerInfo extends JavaPlugin implements Listener {
     	this.getCommand("teamspeak").setExecutor(new TeamSpeakCommand());
     	this.getCommand("website").setExecutor(new WebsiteCommand());
     	this.getCommand("discord").setExecutor(new DiscordCommand());
+    	this.getCommand("twitter").setExecutor(new TwitterCommand()); 
+    	this.getCommand("forums").setExecutor(new ForumsCommand());
+    	this.getCommand("rserverinfo").setExecutor(this);
 }
     
     public FileConfigurationOptions getFileConfigurationOptions()
@@ -43,6 +49,25 @@ public class RServerInfo extends JavaPlugin implements Listener {
     public void onDisable() {
     	System.out.println(CoreConfiguration.GREEN + "§l" + CoreConfiguration.PLUGIN_NAME  + CoreConfiguration.PLUGIN_VERSION + ' ' + CoreConfiguration.DARK_GRAY + "»" + ' ' + CoreConfiguration.GRAY + "This " + CoreConfiguration.GREEN + "§lPLUGIN " + CoreConfiguration.GRAY + "has been " + CoreConfiguration.GREEN + "§lDISABLED" + CoreConfiguration.GRAY + '!');
     }
-    	
-}
+    
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] arg) {
+		if(command.getName().equalsIgnoreCase("rserverinfo")){
+			if(sender.hasPermission("rserverinfo.reload")){
+				if(arg.length < 1){
+					sender.sendMessage(CoreConfiguration.GREEN + CoreConfiguration.PLUGIN_NAME + CoreConfiguration.DARK_GRAY + "»" + CoreConfiguration.GREEN + "§l CREATED" + CoreConfiguration.GRAY + " by" + CoreConfiguration.GREEN + " §lPETTLES" + CoreConfiguration.GRAY + "\nhttps://www.spigotmc.org/resources/rserverinfo.44288/\n" + CoreConfiguration.DARK_GRAY + "» " + CoreConfiguration.GREEN + "/rserverinfo" + CoreConfiguration.DARK_GRAY + " - " + CoreConfiguration.GRAY + "Shows this page!\n" + CoreConfiguration.DARK_GRAY + "» " + CoreConfiguration.GREEN + "/rserverinfo reload" + CoreConfiguration.DARK_GRAY + " - " + CoreConfiguration.GRAY + "Reloads the plugin configuration file!");
+					return false;
+				} if(arg[0].equals("reload")) {
+					Bukkit.getPluginManager().disablePlugin(this);
+					Bukkit.getPluginManager().getPlugin("rServerInfo").reloadConfig();
+					Bukkit.getPluginManager().enablePlugin(this);
+					sender.sendMessage(CoreConfiguration.GREEN + "§l" + CoreConfiguration.PLUGIN_NAME  + ' ' + CoreConfiguration.DARK_GRAY + "»" + ' ' + CoreConfiguration.GRAY + "This " + CoreConfiguration.GREEN + "§lPLUGIN " + CoreConfiguration.GRAY + "has been " + CoreConfiguration.GREEN + "§lRELOADED" + CoreConfiguration.GRAY + '!');
+					return false;
+				}else {
+					sender.sendMessage(CoreConfiguration.GREEN + CoreConfiguration.PLUGIN_NAME + CoreConfiguration.DARK_GRAY + "»" + CoreConfiguration.GREEN + "§l CREATED" + CoreConfiguration.GRAY + " by" + CoreConfiguration.GREEN + " §lPETTLES" + CoreConfiguration.GRAY + "\nhttps://www.spigotmc.org/resources/rserverinfo.44288/\n" + CoreConfiguration.DARK_GRAY + "» " + CoreConfiguration.GREEN + "/rserverinfo" + CoreConfiguration.DARK_GRAY + " - " + CoreConfiguration.GRAY + "Shows this page!\n" + CoreConfiguration.DARK_GRAY + "» " + CoreConfiguration.GREEN + "/rserverinfo reload" + CoreConfiguration.DARK_GRAY + " - " + CoreConfiguration.GRAY + "Reloads the plugin configuration file!");
+					return false;
+					}}
+				}
+		return false;}}
 
+ 
---
+++ src/net/rdevs/plugins/rserverinfo/commands/ForumsCommand.java
@@ -0,0 +1,26 @@
+package net.rdevs.plugins.rserverinfo.commands;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+import net.rdevs.plugins.rserverinfo.CoreConfiguration;
+import net.rdevs.plugins.rserverinfo.RServerInfo;
+
+public class ForumsCommand implements CommandExecutor {
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] arg) {
+		if(RServerInfo.config.getString("Forums_command").equalsIgnoreCase("true")) {
+		sender.sendMessage(ChatColor.translateAlternateColorCodes('&', CoreConfiguration.DARK_GRAY + "&m-|-----------------------------|-"));
+		sender.sendMessage(ChatColor.translateAlternateColorCodes('&', RServerInfo.config.getString("Forums")));
+		sender.sendMessage(ChatColor.translateAlternateColorCodes('&', CoreConfiguration.DARK_GRAY + "&m-|-----------------------------|-"));
+		return false;
+
+} else {
+	if(RServerInfo.config.getString("Forums_command").equalsIgnoreCase("false"))
+		sender.sendMessage(CoreConfiguration.GRAY + "This " + CoreConfiguration.GREEN + "§lCOMMAND" + CoreConfiguration.GRAY + " is " + CoreConfiguration.GREEN + "§ldisabled" + CoreConfiguration.GRAY + '!');
+}
+		return false;
+}}
